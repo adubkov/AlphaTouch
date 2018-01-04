acd672de715ff78f525c75bbb2ceae9d63200600
Alexey Dubkov <alexey.dubkov@gmail.com>
2018-01-04 02:26:07 -0800

    init

---
+++ AlphaTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlphaTouch
+//
+//  Created by Alexey Dubkov on 1/4/18.
+//  Copyright © 2018 Alexey Dubkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
